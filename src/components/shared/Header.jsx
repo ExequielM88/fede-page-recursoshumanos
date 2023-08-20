7ea329d33c62a9442b815545c6b992a022c71ff0
Exequielm88 <kekelom88@gmail.com>
2023-08-19 22:59:30 -0300

    avance

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -5,8 +5,9 @@ import  "./header.css"
 const Header = () => {
 
   const [scrolling, setScrolling] = useState(false);
-
+  const [menuOpen, setMenuOpen] = useState(true); // Estado para controlar el menú desplegable
   // Maneja el cambio de estado cuando haces scroll
+  
   const handleScroll = () => {
     if (window.scrollY > 0) {
       setScrolling(true);
@@ -23,11 +24,19 @@ const Header = () => {
     };
   }, []);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); 
+  }; // Cambia el estado del menú desplegable al hacer clic en el icono
+
+
   return (
     <header className={`header ${scrolling ? "scrolling" : ""}`}>
       <Link to="/"><img className='header-logo' src="../../img/logo.png" alt="" /></Link>
-      <nav className='nav-header'>
-        <ul className='ul-header'>
+      <nav >
+        <div className="menu-icon" onClick={toggleMenu}>
+          ☰
+        </div>
+        <ul className={`ul-header ${menuOpen ? 'open' : ''}`}>
           <li><Link className='link' to="/SobreNosotros">Sobre Nosotros</Link></li>
           <li><Link className='link' to="/NuestrosServicios">Nuestros Servicios</Link></li>
           <li><Link className='link' to="/OportunidadesLaborales">Oportunidades Laborales</Link></li>
